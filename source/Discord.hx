bd5a18e596b95f494f5d4d3b96c847c6dbc63eab
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-08-12 15:37:54 -0300

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -1,3 +1,4 @@
+#if desktop
 package;
 
 import Sys.sleep;
@@ -84,7 +85,7 @@ class DiscordClient
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
-            endTimestamp : Std.int(endTimestamp / 1000)
+                        endTimestamp : Std.int(endTimestamp / 1000)
 		});
 
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
@@ -98,3 +99,4 @@ class DiscordClient
 	}
 	#end
 }
+#end
